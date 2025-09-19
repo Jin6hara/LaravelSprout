0418563b3814d2cfb1f778635371172f6cc9deb3
Jin 6hara <job6hara@gmail.com>
2025-09-19 18:49:33 +0900

    Expense Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
             LessonSeeder::class,
             ScheduleDetailSeeder::class,
             ExpenseReportSeptemberSeeder::class,
+            ExpenseSeptemberBlankSeeder::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
