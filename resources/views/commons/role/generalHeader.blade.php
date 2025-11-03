0aa13e9b9915dfb251a789c59a479b2c19eface7
Jin 6hara <job6hara@gmail.com>
2025-11-03 11:39:53 +0900

    headerのスマホレイアウトを微調整。CSSをpartialize。

--- resources/views/commons/role/generalHeader.blade.php
+++ resources/views/commons/role/generalHeader.blade.php
@@ -1,33 +1,4 @@
 @role('general')
-<style>
-    .header-btn {
-        /* btn-sm の雰囲気を維持しつつ高さを統一 */
-        display: inline-flex;
-        color: #cfcfcfff;
-        align-items: center;
-        justify-content: center;
-        height: 36px;
-        /* ← 統一高さ（必要なら 32〜40px で微調整OK） */
-        padding: 1px 5px !important;
-        /* ←ここを修正 */
-        /* ← 横パディングを 1px に固定 */
-        line-height: 1.1;
-        /* 複数行の詰め調整 */
-        white-space: normal;
-        /* 改行を有効化 */
-        text-align: center;
-        margin: 0 1px;
-    }
-
-    /* li の間隔を少しだけ詰める（任意） */
-    .navbar .nav-item {
-        margin-right: .25rem;
-    }
-
-    .navbar .nav-item:last-child {
-        margin-right: 0;
-    }
-</style>
 
 <li class="nav-item">
     <a class="btn btn-sm btn-outline-secondary header-btn" href="{{ route('calendar.index') }}">
